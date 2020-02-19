f71b289569ca872f0904625af30cc53ec9cc1816
dander94 <3balejandro@gmail.com>
2020-02-19 14:14:25 +0100

    Custom button

---
+++ src/components/buttons/cni-button.vue
@@ -0,0 +1,26 @@
+<template>
+<button class="button" @click="(ev) => $emit('click', ev)">
+  <slot></slot>
+</button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  align-items: center;
+  background-color: $primary-color;
+  color: white;
+  display: flex;
+  justify-content: center;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+}
+</style>
