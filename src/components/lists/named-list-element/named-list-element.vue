8b8a41460c5b154d54bc49f19b60180350604ecd
dander94 <3balejandro@gmail.com>
2020-02-19 14:14:42 +0100

    Named List Element

---
+++ src/components/lists/named-list-element/named-list-element.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="named-list-element">
+  <div class="named-list-element__name">{{ name }}</div>
+  <slot name="action"></slot>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class NamedListElement extends Vue {
+  @Prop()
+  name!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.named-list-element {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
